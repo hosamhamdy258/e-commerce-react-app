48c6b680305151d0a8b6ae9aaa89e9ee32b5532d
hosamhamdy258 <80757319+hosamhamdy258@users.noreply.github.com>
2022-07-12 16:59:13 +0200

    added products list from api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "./store/productsSlice";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import ProductDetails from "./components/ProductDetails";
+import Cart from "./components/Cart";
+import Login from "./components/Login";
+import NotFound from "./components/NotFound";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.productsSlice);
+  console.log(state.products);
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <NavBar />
-      <h1>hello</h1>
-      {/* <Routes>
-        <Route path="/" element={<Home />} />
+      <h1>random text for testing</h1>
+      {/* {state.products &&
+        state.products.map((item) => {
+          return <li key={item.id}>{item.title}</li>;
+        })} */}
+
+      <Routes>
+        <Route path="/" element={<Home productList={state.products} />} />
         <Route path="/home" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<ProductDetails />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
-      </Routes> */}
+      </Routes>
       <Footer />
     </div>
   );
