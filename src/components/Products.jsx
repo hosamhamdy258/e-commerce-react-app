016d3f78021e6b5ba4982d52f7a8e755079f5826
hosamhamdy258 <80757319+hosamhamdy258@users.noreply.github.com>
2022-07-18 10:51:45 +0200

    Revert "Merge pull request #9 from hosamhamdy258/maha"
    This reverts commit 47f853e6b41f4968a9b83e40fb533979a6021c59, reversing
    changes made to 419a085c6ebf9a1d1f1da9ba87f46bb197d1560b.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,22 +1,6 @@
 
 import React from "react";
-import Header from "./Maha/Header";
-import "./Maha/main.css";
-import Upper from "./Maha/Upper";
-import FilteredProductsShow from './Maha/FilteredProductsShow';
 
 export default function Products({ productList }) {
-  return (
-    <>
-      <Header />
-      <Upper
-        productList={productList.filter(function (item) {
-          return item.discountPercentage > 13;
-        })}
-      />
-      <h1 className="style_Pro">Products</h1>
-      <FilteredProductsShow productList={productList}/>
-     
-    </>
-  );
+  return <div>Products</div>;
 }
